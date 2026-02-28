cc28012c78112bb7076ef5fe43d4e46d00cc492e
Karamveer Garathi <karamveer.garathi.9@gmail.com>
2026-02-28 16:57:06 +0530

    cors policy updated

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,10 @@ const cors = require("cors");
 const port = process.env.PORT;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 app.use(admin_routes);
 
